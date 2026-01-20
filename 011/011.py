9ac9a2168ffe4586b55fa196b4b31394d3d803d5
Gustavo Almeida <gu.almeidan2007@gmail.com>
2026-01-20 08:52:43 -0300

    Add Python script to calculate the amount of paint needed for a wall

---
+++ 011/011.py
@@ -0,0 +1,8 @@
+largura = float(input("digite a largura da parede: "))
+altura = float(input("digite a altura da parede: "))
+
+metrosCubicos = (largura * altura)
+
+metrosCubicosPintados = (metrosCubicos * 2)
+
+print (f"A quantidade de tinta necessária para pintar a parede é {metrosCubicosPintados} litros")
