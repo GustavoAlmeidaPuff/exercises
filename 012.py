43fcee9a3a2f1550338fb6d5766d487aa60cba58
Gustavo Almeida <gu.almeidan2007@gmail.com>
2026-01-21 14:16:37 -0300

    Add Python script to calculate the new price of a product after applying a 5% discount

--- 012.py
+++ 012.py
@@ -2,3 +2,14 @@
 #
 # ### Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 
+# Solicita o preço do produto ao usuário
+preco = float(input('Digite o preço do produto: '))
+
+# Calcula o desconto de 5%
+desconto = preco * 0.05
+
+# Calcula o novo preço com o desconto
+novo_preco = preco - desconto
+
+# Exibe o novo preço com 2 casas decimais
+print(f'O preço do produto com 5% de desconto é: {novo_preco:.2f}')
