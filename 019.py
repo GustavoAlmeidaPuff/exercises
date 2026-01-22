c21c54645c56d43bdc4060e4a53f3ef343fc7cc4
Gustavo Almeida <gu.almeidan2007@gmail.com>
2026-01-22 10:50:55 -0300

    Exercise 019

--- 019.py
+++ 019.py
@@ -1,3 +1,11 @@
 # ## 019
 #
 # ### Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido.
+
+import random
+
+alunos = ['João', 'Maria', 'Pedro', 'Ana'] # lista de alunos
+
+escolhido = random.choice(alunos) # escolhe um aluno aleatório usando a função choice da biblioteca random
+
+print(f'O aluno escolhido foi: {escolhido}') # exibe o aluno escolhido usando a função format da biblioteca random
