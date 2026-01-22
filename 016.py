dce13e13afb74efc89023586c92eab61d759fd26
Gustavo Almeida <gu.almeidan2007@gmail.com>
2026-01-22 10:43:46 -0300

    Add Python script to read a real number and display its integer part

--- 016.py
+++ 016.py
@@ -1,3 +1,7 @@
 # ## 016
 #
 # ### Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
+
+numero = float(input('Digite um número real: '))
+
+print(f'O número {numero} tem a parte inteira {int(numero)}')
