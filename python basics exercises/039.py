1a78038405f04d376c34b57cfe5aa4a9fbff6ebe
Gustavo Almeida <gu.almeidan2007@gmail.com>
2026-04-15 18:09:15 -0300

    Implement Exercise 039: Create a program that reads a person's birth year and determines their military enlistment status based on their age, indicating whether they still need to enlist, if it's the right time, or if the deadline has passed.

--- python basics exercises/039.py
+++ python basics exercises/039.py
@@ -1,3 +1,14 @@
 # ## 039
 #
 # ### Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com a sua idade, se ele ainda vai se alistar ao serviço militar, se é a hora exata de se alistar ou se já passou do tempo do alistamento. Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
+
+ano_nascimento = int(input("Digite o ano de nascimento: "))
+
+idade = 2026 - ano_nascimento
+
+if idade < 18:
+    print("Você ainda vai se alistar ao serviço militar")
+elif idade == 18:
+    print("É a hora exata de se alistar")
+else:
+    print("Você já passou do tempo do alistamento")
