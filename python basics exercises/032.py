10d65afd2796b42f79bb09beb158cd9ed53af250
Gustavo Almeida <gu.almeidan2007@gmail.com>
2026-04-15 17:30:38 -0300

    Implement Exercise 032: Create a program that reads a year and determines if it is a leap year based on specific divisibility rules.

--- python basics exercises/032.py
+++ python basics exercises/032.py
@@ -1,3 +1,10 @@
 # ## 032
 #
 # ### Faça um programa que leia um ano qualquer e mostre se ele é bissexto.
+
+ano = int(input("Digite um ano qualquer: "))
+
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:  # É bissexto se for divisível por 4 e não por 100, ou se for divisível por 400.
+    print("O ano é bissexto")
+else:
+    print("O ano não é bissexto")
