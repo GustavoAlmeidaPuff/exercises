ecb8385486e94f6e591f44d308ded8187daf3f38
Gustavo Almeida <gu.almeidan2007@gmail.com>
2026-04-15 17:50:58 -0300

    Implement Exercise 036: Create a program to approve a bank loan for house purchase based on the house value, buyer's salary, and repayment period, ensuring monthly payments do not exceed 30% of the salary.

--- python basics exercises/036.py
+++ python basics exercises/036.py
@@ -1,3 +1,16 @@
 # ## 036
 #
 # ### Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. Pergunte o valor da casa, o salário do comprador e em quantos anos ele vai pagar. A prestação mensal não pode exceder 30% do salário ou então o empréstimo será negado.
+
+valor_casa = float(input("Digite o valor da casa: "))
+salario = float(input("Digite o salário do comprador: "))
+anos = int(input("Digite em quantos anos o comprador vai pagar: "))
+
+valor_prestacao = valor_casa / (anos * 12)
+
+if valor_prestacao > salario * 0.30:
+    print("Empréstimo NEGADO!")
+else:
+    print("Empréstimo APROVADO!")
+
+print(f"A prestação mensal é de R${valor_prestacao:.2f}, isso é {valor_prestacao / salario * 100}% do salário do comprador.")
