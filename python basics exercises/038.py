9e5b4bfaa5518adc2c86259e035dc910eaeb3ce9
Gustavo Almeida <gu.almeidan2007@gmail.com>
2026-04-15 18:06:55 -0300

    Implement Exercise 038: Create a program that reads two integers and compares them, displaying a message indicating which number is greater or if they are equal.

--- python basics exercises/038.py
+++ python basics exercises/038.py
@@ -1,3 +1,14 @@
 # ## 038
 #
 # ### Escreva um programa que leia dois números inteiros e compare-os. mostrando na tela uma mensagem:
+
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+
+if numero1 > numero2:
+    print("O primeiro número é maior")
+elif numero2 > numero1:
+    print("O segundo número é maior")
+else:
+    print("Os dois números são iguais")
+
