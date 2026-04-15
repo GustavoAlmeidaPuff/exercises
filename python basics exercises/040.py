c740633dcb4acf75fd660dcd7c16be18f6dd086d
Gustavo Almeida <gu.almeidan2007@gmail.com>
2026-04-15 18:12:51 -0300

    Implement Exercise 040: Create a program that reads two grades from a student, calculates the average, and displays a message indicating whether the student is approved, in recovery, or failed based on the average score.

--- python basics exercises/040.py
+++ python basics exercises/040.py
@@ -1,3 +1,15 @@
 # ## 040
 #
 # ### Crie um programa que leia duas notas de um aluno e calcule sua média, mostrando uma mensagem no final, de acordo com a média atingida:
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+
+media = (nota1 + nota2) / 2
+
+if media < 5:
+    print("Reprovado")
+elif media >= 5 and media < 7:
+    print("Recuperação")
+else:
+    print("Aprovado")
