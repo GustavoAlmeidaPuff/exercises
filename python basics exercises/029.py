aab1c6e1f66a126e7ed2a3e6099f83f80a5d37af
Gustavo Almeida <gu.almeidan2007@gmail.com>
2026-04-15 17:08:25 -0300

    Implement Exercise 029: Create a program that reads a car's speed and calculates a fine if it exceeds 80Km/h, with a fine of R$7.00 for each Km over the limit.

--- python basics exercises/029.py
+++ python basics exercises/029.py
@@ -1,3 +1,11 @@
 # ## 029
 #
 # ### Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80Km/h, mostre uma mensagem dizendo que ele foi multado. A multa vai custar R$7,00 por cada Km acima do limite.
+
+velocidade = float(input("Digite a velocidade do carro: "))
+
+if velocidade > 80:
+    multa = (velocidade - 80) * 7
+    print(f"Você foi multado em R${multa:.2f}")
+else:
+    print("Você não foi multado")
